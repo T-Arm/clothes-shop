9a2dc5e23775570bd1c08668ca2005e5657a757b
tfire7 <tdarmstrong03@gmail.com>
2020-05-31 17:48:25 -0400

    started the project, made fromt page menu and first link.

---
+++ src/pages/hatspage/hatsPage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './hatsPage.style.scss';
+
+const HatsPage = () => (
+  <div className= 'hats-page'>
+    <h1>hats page</h1>
+  </div>
+)
+
+export default HatsPage;
